c46ec2f33e0ba1372744d1e38f89fa470f454389
Brandon Avant <brandon.avant@ruralsourcing.com>
2021-03-04 16:03:36 -0600

    Initial Commit

---
+++ AccountsAPI/AccountsAPI.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountsAPI.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ AccountsAPI/AccountsAPI.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountsAPI.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
