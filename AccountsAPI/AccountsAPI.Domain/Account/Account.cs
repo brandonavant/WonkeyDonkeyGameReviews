09c4a8ed27fe751bd13f6a10806b19b9d5d6a585
Brandon Avant <brandon.avant@ruralsourcing.com>
2021-04-01 17:14:10 -0500

    Added todo comments

--- AccountsAPI/AccountsAPI.Domain/Account/Account.cs
+++ AccountsAPI/AccountsAPI.Domain/Account/Account.cs
@@ -8,6 +8,8 @@ namespace AccountsAPI.AccountsAPI.Domain.Account
     
     public class Account : AggregateRoot<Account, Guid>
     {
+        // TODO: Add props
+        
         private Account()
         {
             
@@ -15,6 +17,7 @@ namespace AccountsAPI.AccountsAPI.Domain.Account
 
         public Account Create(IAccountUniquenessChecker uniquenessChecker)
         {
+            // TODO: Talk about (as a group) what uniqueness is and write unit tests to support.
             CheckRule(new AccountIsUniqueRule(uniquenessChecker));
 
             return new Account();
